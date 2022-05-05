41e848f0bd68c325c2b8c8e96cc8d198f39d0c53
mohwildan <mobafreewtf@gmail.com>
2022-05-05 20:57:13 +0700

    seli

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env:{
     PROD_URL: process.env.PROD_URL,
-    DEV_URL: process.env.DEV_URL 
+    DEV_URL: process.env.DEV_URL ,
+    MONGO_URI: process.env.MONGO_URI
   }
 }
 
--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -10,7 +10,7 @@ return;
 
 }
 
-const db = await mongoose.connect("mongodb://localhost:27017/Baru_db", {
+const db = await mongoose.connect(process.env.MONGO_URI, {
 
 useNewUrlParser: true,
 
